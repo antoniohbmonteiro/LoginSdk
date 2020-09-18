9f5fcdbb7bce66d2091c7e8f42498e03e01cee8f
Antonio Monteiro <antoniomonteiro@ioasys.com.br>
2020-09-18 12:45:34 -0300

    Basic Navigation

---
+++ app/src/main/java/com/antoniomonteiro/loginsdk/LoginFragment.kt
@@ -0,0 +1,29 @@
+package com.antoniomonteiro.loginsdk
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        signupBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
+        }
+    }
+
+}
