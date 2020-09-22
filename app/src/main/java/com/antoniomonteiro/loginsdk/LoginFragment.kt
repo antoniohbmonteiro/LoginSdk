a195d6c92c1e98b2cf5024ae4c7c47245c13abbe
Antonio Monteiro <antoniohbmonteiro@gmail.com>
2020-09-22 15:03:15 -0300

    Adicionado retorno de navegação

--- app/src/main/java/com/antoniomonteiro/loginsdk/LoginFragment.kt
+++ app/src/main/java/com/antoniomonteiro/loginsdk/LoginFragment.kt
@@ -2,6 +2,7 @@ package com.antoniomonteiro.loginsdk
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -27,7 +28,9 @@ class LoginFragment : Fragment() {
         }
 
         loginBtn.setOnClickListener {
+            Log.i("AntonioTesteTag", "funfou")
             activity?.setResult(Activity.RESULT_OK)
+            activity?.finish()
         }
     }
 
